eb0fac02f4e96dca5777ebae6dfe427b86ff985f
mksdv <46026355+mksdv@users.noreply.github.com>
2022-03-31 20:34:22 +0200

    feat(logger): add shortcuts for `fmt.Sprintf`

--- logger.go
+++ logger.go
@@ -1,5 +1,7 @@
 package lumberjack
 
+import "fmt"
+
 type Logger struct{
   cfg Config
   ctx *entry
@@ -72,18 +74,38 @@ func (l *Logger) Fatal(msg string) *entry {
   return e
 }
 
+func (l *Logger) Fatalf(format string, a ...any) *entry {
+  return l.Fatal(fmt.Sprintf(format, a))
+}
+
 func (l *Logger) Error(msg string) *entry {
   return l.Log(LevelError, msg)
+}
+
+func (l *Logger) Errorf(format string, a ...any) *entry {
+  return l.Error(fmt.Sprintf(format, a))
 }
 
 func (l *Logger) Warn(msg string) *entry {
   return l.Log(LevelWarn, msg)
 }
 
+func (l *Logger) Warnf(format string, a ...any) *entry {
+  return l.Warn(fmt.Sprintf(format, a))
+}
+
 func (l *Logger) Info(msg string) *entry {
   return l.Log(LevelInfo, msg)
 }
 
+func (l *Logger) Infof(format string, a ...any) *entry {
+  return l.Info(fmt.Sprintf(format, a))
+}
+
 func (l *Logger) Debug(msg string) *entry {
   return l.Log(LevelDebug, msg)
+}
+
+func (l *Logger) Debugf(format string, a ...any) *entry {
+  return l.Debug(fmt.Sprintf(format, a))
 }
